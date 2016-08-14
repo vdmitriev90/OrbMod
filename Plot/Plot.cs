af043f16998b571b38c01787904208d49cdc4826
vdmitriev90 <vm.dmitriev90@gmail.com>
2016-08-14 22:18:22 +0300

    plot resultы 2

--- Plot/Plot.cs
+++ Plot/Plot.cs
@@ -24,35 +24,9 @@ namespace OrbModUI
 
         public abstract void PlotData();
         public abstract void EndPlot();
+
         public abstract void Autoscale();
-    }
-     abstract class SingePlot : Plot
-    {
-        public SingePlot()
-        {
-
-        }
-        public SingePlot(ZedGraphControl zg): base(zg)
-        {
-
-        }
-        protected abstract bool AddPoint(string str, PointPairList list);
-
-        public override void PlotData()
-        {
-            GraphPane pane = zg.GraphPane;
-            PointPairList points = new PointPairList();
-
-            using (StringReader sr = new StringReader(FName))
-            {
-                while(sr.Peek()!=-1)
-                {
-                    string line = sr.ReadLine();
-                    AddPoint(line, points);
-                }
-                EndPlot();
-            }
-        }
 
     }
+    
 }
