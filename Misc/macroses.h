785726e59a9c2e1449dd429a8b9db14b1090c225
vdmitriev90 <vm.dmitriev90@gmail.com>
2016-09-22 10:11:24 +0300

    vcxproj added

--- Misc/macroses.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include"stdafx.h"
-
-#define PI 3.14159265358979323846
-#define SQR(val) ((val) * (val))
-#define CUB(val) ((val) * (val) * (val))
-#define N_MIN(A,B) ((A)<(B)?(A):(B))
-#define N_MAX(A,B) ((A)>(B)?(A):(B))
--- Misc/visibility.h
+++
@@ -1,8 +0,0 @@
-
-#pragma once
-#include <stdio.h>
-#include "stdafx.h"
-namespace OrbMod
-{
-	void visi(FILE*fvisi, SpiceDouble t0, triple X, triple V);
-}
